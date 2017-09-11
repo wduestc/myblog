36c426c6c487cff7d5fca57906ec272b5d8335ed
w_di_sc <wduestc@163.com>
2017-09-11 10:01:13 +0800

    修改编辑页面的函数

--- blog/views.py
+++ blog/views.py
@@ -14,12 +14,28 @@ def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article':article})
 
-def edit_page(request):
-    return render(request, 'blog/edit_page.html')
+def edit_page(request, article_id):
+    if str(article_id) == "0":
+        return render(request, 'blog/edit_page.html')
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        return render(request, 'blog/edit_page.html', {'article':article})
 
 def edit_action(request):
-    title = request.POST.get('title', 'TITLE')
-    content = request.POST.get('content', 'CONTENT')
-    models.Article.objects.create(title=title, content=content)
-    articles = models.Article.objects.all()
+    flag = request.POST.get('flag', '0')
+    if flag == '0':
+      title = request.POST.get('title', 'TITLE')
+      content = request.POST.get('content', 'CONTENT')
+      models.Article.objects.create(title=title, content=content)
+      articles = models.Article.objects.all()
+    elif flag == '1':
+      #edit article
+      id = request.POST.get('id', '0')
+      title = request.POST.get('title', 'TITLE')
+      content = request.POST.get('content', 'CONTENT')
+      article = models.Article.objects.get(pk=id)
+      article.title = title
+      article.content = content
+      article.save()
+      articles = models.Article.objects.all()
     return render(request, 'blog/index.html', {'articles':articles})
